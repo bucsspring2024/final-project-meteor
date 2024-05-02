5eed463ca61b71bce2799569904f792ca533c37a
Ben Lee <clee88@binghamton.edu>
2024-05-01 21:21:59 -0700

    Fianl_Project_Chanwoo_Lee

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import pygame
-#import your controller
+from meteor_protector import MeteorProtector
+from meteor_protector import GameIntro
 
 def main():
     pygame.init()
-    #Create an instance on your controller object
-    #Call your mainloop
-    
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
+    game = MeteorProtector(1400, 800)
+    game.main()
 
-# https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
     main()
